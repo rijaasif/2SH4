e7ded7b5b6da2c6545f6582cc70f32be8389639c
velvid <velvid>
2019-10-08 13:56:46 -0400

    working on Q2

---
+++ Lab3/ali_muntakim_400210016_lab3_L07_question2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+student **create_class_list(char *fileName, int *sizePtr);
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
+
+student **create_class_list(char *fileName, int *sizePtr) {
+
+}
+
+// struct student {
+//     int studentID;
+//     char firstName[20];
+//     char lastName[20];
+//     int proj1Grade;
+//     int proj2Grade;
+//     float finalGrade;
+// }
