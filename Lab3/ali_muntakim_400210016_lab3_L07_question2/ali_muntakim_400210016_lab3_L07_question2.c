cc0802314ca3743918077c16d0cf6e15d09f345d
velvid <muntakim.ca@gmail.com>
2019-10-12 23:44:36 -0400

    finished input_grades

--- Lab3/ali_muntakim_400210016_lab3_L07_question2/ali_muntakim_400210016_lab3_L07_question2.c
+++ Lab3/ali_muntakim_400210016_lab3_L07_question2/ali_muntakim_400210016_lab3_L07_question2.c
@@ -26,25 +26,18 @@ int main() {
     int size;           // class size or # of students
     student **list;     // pointer to the beginning of array of pointers of each student
 
+    /* create the class list */
     list = create_class_list("class_list.txt", &size);
-
-    /* prints contents of list */
-    printf("%d\n", size);
-    int i;
-    for(i = 0; i < size; i++) {
-        printf("%d\t%s %s\t%d %d %.2f\n",
-               list[i]->studentID,
-               list[i]->firstName,
-               list[i]->lastName,
-               list[i]->proj1Grade,
-               list[i]->proj2Grade,
-               list[i]->finalGrade);
-    }
-    printf("\n");
+    print_list(list, size);
 
     /* testing the find function */
     printf("index of %d is %d\n", 2512, find(2512, list, size));
     printf("index of %d is %d\n", 1200, find(1200, list, size));
+    printf("\n");
+
+    /* input grades into list */
+    input_grades("project_grades.txt", list, size);
+    print_list(list, size);
 
 
 
@@ -55,6 +48,13 @@ int main() {
 /* function reads the input file and puts elements into an array of structures */
 student **create_class_list(char *filename, int *sizePtr) {
     FILE *filePtr = fopen(filename, "r");   // file pointer to filename
+
+    /* exit code for if file does not exist */
+    if(filePtr == NULL) {
+        printf("ERROR: the file '%s' does not exist\n", filename);
+        exit(1);
+    }
+
     fscanf(filePtr, "%d", sizePtr);         // gets # of students from first line
 
     /* allocates memory for entire student list for the **list variable */
@@ -65,13 +65,13 @@ student **create_class_list(char *filename, int *sizePtr) {
     for(i = 0; i < *sizePtr; i++) {
         /* new allocation for each student struct, auto-sets values to 0*/
         list[i] = calloc(1, sizeof(student));
-        fscanf(filePtr, "%d %s %s",     // reads each line of the file
-               &list[i]->studentID,     // scans studentID
-               &list[i]->firstName,     // scans firstName
-               &list[i]->lastName);     // scans lastName
+        fscanf(filePtr, "%d %s %s",     // scans each line of the file
+            &list[i]->studentID,        // puts 1st num into studentID
+            &list[i]->firstName,        // puts 2nd str into firstName
+            &list[i]->lastName);        // puts 3rd str into lastName
     }
 
-    fclose(filePtr);    //always close file after reading
+    fclose(filePtr);    // always close file after reading
     return list;
 }
 
@@ -80,25 +80,41 @@ int find(int idNo, student **list, int size) {
     int i;
     for(i = 0; i < size; i++) {
         if(list[i]->studentID == idNo) {
-            return i;   //return index if idNo matches
+            return i;   // return index if idNo matches
         }
     }
-    return -1;  //return -1 by default (if idNo is not in list)
+    return -1;  // return -1 by default (if idNo is not in list)
 }
 
 void input_grades(char *filename, student **list, int size) {
-    FILE *filePtr = fopen(filename, "r");
-    int idNo, pos;
+    FILE *filePtr = fopen(filename, "r");   // file pointer to file name
+    int idNo, pos;  // student id number and index position respectively
+
+    /* exit code for if file does not exist */
+    if(filePtr == NULL) {
+        printf("ERROR: the file '%s' does not exist\n", filename);
+        exit(1);
+    }
+
+    /* iterates through 'project_grades.txt' and adds grades to student list */
     int i;
     for(i = 0; i < size; i++) {
-        fscanf(filePtr, "%d", idNo);
-        pos = find(idNo, )
+        fscanf(filePtr, "%d", &idNo);   // scans the student id number
+        pos = find(idNo, list, size);   // finds the index of student id
+        if(pos == -1) {
+            /* print error and exit if id number is not in file */
+            printf("ERROR: student in 'project_grades.txt' is not in 'class_list.txt'\n");
+            exit(1);
+        } else {
+            fscanf(filePtr, "%d %d",
+                &list[pos]->proj1Grade,     // assign project 1 grade into list
+                &list[pos]->proj2Grade);    // assign project 2 grade into list
+        }
     }
 }
 
-
-void compute_final_course_grades() {
-
+void compute_final_course_grades(student **list, int size) {
+    
 }
 
 void output_final_course_grades() {
@@ -106,7 +122,17 @@ void output_final_course_grades() {
 }
 
 void print_list(student **list, int size) {
-
+    int i;
+    for(i = 0; i < size; i++) {
+        printf("%d\t%s %s\t%3d %3d %6.2f\n",
+            list[i]->studentID,
+            list[i]->firstName,
+            list[i]->lastName,
+            list[i]->proj1Grade,
+            list[i]->proj2Grade,
+            list[i]->finalGrade);
+    }
+    printf("\n");
 }
 
 void withdraw(int idNo, student **list, int *sizePtr) {
