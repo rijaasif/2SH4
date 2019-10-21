13e6bd9ec2284a3da4a993e8544a0b8681f55ffd
velvid <muntakim.ca@gmail.com>
2019-10-21 13:27:37 -0400

    working on bonus

--- Lab3/ali_muntakim_400210016_lab3_L07_question3/ali_muntakim_400210016_lab3_L07_question3.c
+++ Lab3/ali_muntakim_400210016_lab3_L07_question3/ali_muntakim_400210016_lab3_L07_question3.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_LEN 256
-
-/* allowed to use strlen() and strcpy() */
 
 char **read_words(const char *filename, int *nPtr);
 void print_words(char **words, int size);
 void sort_list(char **words, int size);
+void free_words(char **words, int *size);
 
 int main() {
     int n = 0;
 
     char **words = read_words("input_file.txt", &n);
 
-    printf("BEFORE SORTING:\n");
-    print_words(words, n);
+//    printf("BEFORE SORTING:\n");
+//    print_words(words, n);
 
-//    sort_list(words, n);
+    sort_list(words, n);
 //    printf("AFTER SORTING:\n");
 //    print_words(words, n);
+
+    free_words(words, &n);
 
     return 0;
 }
 
 char **read_words(const char *filename, int *nPtr) {
-    FILE *filePtr = fopen(filename, "r");   //opens file to read
+    FILE *filePtr = fopen(filename, "r");
     char **words;
     char word[MAX_LEN];
     int total_char = 0;
@@ -41,17 +43,13 @@ char **read_words(const char *filename, int *nPtr) {
     }
     rewind(filePtr);
 
-    words = malloc(total_char * sizeof(char));
+    words = malloc(*nPtr * sizeof(char*));
 
     int i;
     for(i = 0; i < *nPtr; i++) {
         fscanf(filePtr, "%s", word);
         words[i] = malloc(strlen(word) * sizeof(char));
-    }
-    rewind(filePtr);
-
-    for(i = 0; i < *nPtr; i++) {
-        fscanf(filePtr, "%s", words[i]);
+        strcpy(words[i], word);
     }
 
     fclose(filePtr);
@@ -67,17 +65,36 @@ void print_words(char **words, int size) {
     printf("\n");
 }
 
-//void sort_list(char **words, int size) {
-//    char *temp;
-//    int i, j;
-//    for(i = 0; i < size-1; i++) {
-//        j = i;
-//        for(j = i; j > 0 && words[j-1][0] > words[j][0]; j--) {
-//            /* swap words[j] and words[j-1] */
-//            temp = (char*) malloc((strlen(words[j-1]))*sizeof(char));
-//            strcpy(temp, words[j-1]);
-//            strcpy(words[j-1], words[j]);
-//            strcpy(words[j], temp);
-//        }
-//    }
-//}
+void sort_list(char **words, int size) {
+    char *temp;
+    int letter = 0;
+    int i, j, k;
+    for(i = 1; i < size-1; i++) {
+        for(j = i+1; j > 0 && words[j-1][letter] > words[j][letter]; j--) {
+            /* swap words[j] and words[j-1] */
+            temp = malloc((strlen(words[j-1])) * sizeof(char));
+            strcpy(temp, words[j-1]);
+
+            words[j-1] = realloc(words[j-1], sizeof(words[j]));
+            strcpy(words[j-1], words[j]);
+
+            words[j] = realloc(words[j], sizeof(temp));
+            strcpy(words[j], temp);
+
+            free(temp);
+
+            printf("i=%d j=%d\n", i, j);
+            print_words(words, size);
+        }
+    }
+
+}
+
+void free_words(char **words, int *size) {
+    int i;
+    for(i = 0; i < *size; i++) {
+        free(words[i]);
+    }
+    *words = NULL;
+    *size = 0;
+}
