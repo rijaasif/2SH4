010442463120515bf160d214eac31546422fb182
velvid <muntakim.ca@gmail.com>
2019-10-30 23:35:31 -0400

    added user input

--- Lab3/ali_muntakim_400210016_lab3_L07_question3/ali_muntakim_400210016_lab3_L07_question3.c
+++ Lab3/ali_muntakim_400210016_lab3_L07_question3/ali_muntakim_400210016_lab3_L07_question3.c
@@ -7,24 +7,67 @@
 char **read_words(const char *filename, int *nPtr);
 void print_words(char **words, int size);
 void swap_words(char *word1, char *word2);
-void sort_list(char **words, int size);
+void insertion_sort_list(char **words, int size);
+void quick_sort_list(char **words, int size);
 void free_words(char **words, int *size);
 
+void instructions() {
+    printf("Enter 'prompt' command numbers:\n");
+    printf("0. exit program\n");
+    printf("1. create word list\n");
+    printf("2. print words\n");
+    printf("3. insertion sort\n");
+    printf("4. quick sort\n");
+    printf("5. destroy list\n\n");
+}
+
 int main() {
-    int n = 0;
+    int n = 0;      //size of word list
+    char **words;   //pointer to pointers/arrays
 
-    char **words = read_words("input_file.txt", &n);
+    instructions(); // print instructions for user
 
-    printf("BEFORE SORTING:\n");
-    print_words(words, n);
-
-    sort_list(words, n);
-
-    printf("AFTER SORTING:\n");
-    print_words(words, n);
-
-    free_words(words, &n);
-
+    /* infinite loop to keep asking user for prompts */
+    int prompt;
+    while(1) {
+        printf("Enter prompt: ");
+        scanf("%d", &prompt);
+        switch(prompt) {
+            case 0:     // allows user to exit the program
+                printf("\nExiting program...\n");
+                return 0;
+                break;
+            case 1:
+                printf("Creating word list...");
+                words = read_words("input_file.txt", &n);
+                printf("done.\n");
+                break;
+            case 2:
+                printf("\n");
+                print_words(words, n);
+                break;
+            case 3:
+                printf("Sorting list using INSERTION...");
+                insertion_sort_list(words, n);
+                printf("done.\n");
+                break;
+            case 4:
+                printf("Sorting list using QUICK...");
+                quick_sort_list(words, n);
+                printf("done.\n");
+                break;
+            case 5:
+                printf("Destroying list in memory...");
+                free_words(words, &n);
+                printf("done.\n");
+                break;
+            default:
+                printf("\n");
+                instructions(); // print instructions if user enters wrong prompt
+                break;
+        }
+        printf("\n");
+    }
     return 0;
 }
 
@@ -35,7 +78,7 @@ char **read_words(const char *filename, int *nPtr) {
 
     /* exit program if file is not found */
     if(filePtr == NULL) {
-        printf("ERROR: the file '%s' does not exist", filename);
+        printf("\nERROR: The file '%s' does not exist.\n", filename);
         exit(1);
     }
 
@@ -49,7 +92,7 @@ char **read_words(const char *filename, int *nPtr) {
     int i;
     for(i = 0; i < *nPtr; i++) {
         fscanf(filePtr, "%s", word);
-        words[i] = malloc((strlen(word)+1) * sizeof(char));
+        words[i] = malloc((strlen(word)+1) * sizeof(char));     // add +1 for '\n' terminator
         strcpy(words[i], word);
     }
 
@@ -64,16 +107,16 @@ void print_words(char **words, int size) {
     for(i = 0; i < size; i++) {
         printf("%s\n", words[i]);
     }
-    printf("\n");
 }
 
-void sort_list(char **words, int size) {
+/* uses insertion sort method to sort the list */
+void insertion_sort_list(char **words, int size) {
     char *temp;
     int letter = 0;
     int i, j;
     for(i = 0; i < size-1; i++) {
         for(j = i+1; j > 0 && words[j-1][letter] > words[j][letter]; j--) {
-            //swap words
+            // swap words
             temp = words[j-1];
             words[j-1] = words[j];
             words[j] = temp;
@@ -81,6 +124,21 @@ void sort_list(char **words, int size) {
     }
 }
 
+void quick_sort_list(char **words, int size) {
+    char *temp;
+    int letter = 0;
+    int i, j;
+    for(i = 0; i < size-1; i++) {
+        for(j = i+1; j > 0 && words[j-1][letter] > words[j][letter]; j--) {
+            // swap words
+            temp = words[j-1];
+            words[j-1] = words[j];
+            words[j] = temp;
+        }
+    }
+}
+
+/* frees each element in the pointer array and resets all variables */
 void free_words(char **words, int *size) {
     int i;
     for(i = 0; i < *size; i++) {
